39afc38bef40f738032ab8b9169caa7b198c5ad6
SusiIrwin <susi.irwin@gmail.com>
2016-09-04 11:23:18 -0600

    adds user story and feature test for tutorial portion of i4

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,26 @@
+class PlaylistsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @playlist = Playlist.new
+    @songs = Song.all
+  end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+    redirect_to @playlist
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(:name, song_ids: [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   end
 
   resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show]
 
 end
