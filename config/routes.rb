2b5ab31cd8f39c773147ac62807eb449a30a1feb
SusiIrwin <susi.irwin@gmail.com>
2016-09-03 20:39:59 -0600

    completes I3.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :artists
+  resources :artists do
+    resources :songs, only: [:new, :create]
+  end
+
+  resources :songs, only: [:show]
 
 end
--- spec/features/user_creates_a_song_spec.rb
+++ spec/features/user_creates_a_song_spec.rb
@@ -1,17 +1,17 @@
-require 'rails-helper'
+require 'rails_helper'
 
-RSpec.feature "User submits a song" do
-  scenario "they see the page for the song" do
+RSpec.feature "User submits a new song" do
+  scenario "they see the page for the individual song" do
     artist = create(:artist)
 
     song_title = "One Love"
 
     visit artist_path(artist)
-    click_on "New Song"
+    click_on "New song"
     fill_in "song_title", with: song_title
     click_on "Create Song"
 
     expect(page).to have_content song_title
     expect(page).to have_link artist.name, href: artist_path(artist)
   end
-end
+end 
