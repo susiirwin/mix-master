9940a69f05352a168cb4fb0337d3c67e54ac6ce5
SusiIrwin <susi.irwin@gmail.com>
2016-09-04 21:11:23 -0600

    adds rack tests as prescribed by tutorial

---
+++ spec/controllers/artists_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe ArtistsController, type: :controller do
+  describe "GET #index" do
+    it "assigns all artists as @artists and renders the index template" do
+      artist = create(:artist)
+
+      get(:index)
+
+      expect(assigns(:artists)).to eq([artist])
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested artist as @artist and renders the show template" do
+      artist = create(:artist)
+      get(:show, {:id => artist.to_param})
+      expect(assigns(:artist)).to eq(artist)
+      expect(response).to render_template("show")
+    end
+  end
+end
